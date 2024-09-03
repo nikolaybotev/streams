6e7ff1a4504641a13a99702eef72edbc01ccd5fd
Nikolay Botev <nikolaybotev.b@gmail.com>
2024-09-03 16:13:18 +0300

    Initial unit tests with actual expectations.

--- src/test.ts
+++ src/test.ts
@@ -1,60 +1,145 @@
-test("a", () => {
-  function* x(g: (_: number) => number = _ => _): Generator<number> {
-    console.log("begin");
-    yield g(0);
-    console.log("end");
+function logger() {
+  const output: unknown[] = [];
+
+  const log = (...s: unknown[]) => {
+    output.push(...s);
+    console.log(...s);
+  }
+
+  log.output = output;
+
+  return log;
+}
+
+test("generator without return statement has expected order of operations", () => {
+  const log = logger();
+  function* x(): Generator<number> {
+    log("begin");
+    yield 0;
+    log("end");
   }
 
   const a = x();
+  log("gen");
+  log(a.next());
+  log(a.next());
+  log("donee");
 
-  console.log("gen");
-  console.log(a.next());
-  console.log(a.next());
-  console.log("donee");
+  expect(log.output).toEqual([
+    "gen",
+    "begin",
+    { value: 0, done: false },
+    "end",
+    { value: undefined, done: true },
+    "donee"
+  ])
 });
 
-test("b", async () => {
-  function* x(g: (_: number) => number = _ => _): Generator<number> {
+test("generator completes before exception is handled", async () => {
+  const log = logger();
+
+  function* x(): Generator<number> {
     try {
-      console.log("begin");
-      yield g(0);
-      console.log("middle");
-      yield g(1);
-      console.log("end");
+      log("begin");
+      yield 0;
+      log("middle");
+      yield 1;
+      log("end");
     } catch (e) {
-      console.log("ERROR", e);
+      log("ERROR", e);
     } finally {
-      console.log("finally");
+      log("finally");
     }
   }
+
   let c = 0;
   try {
     for await (const n of x()) {
-      console.log(`GOT ${c} elem`);
+      log(`GOT ${c} elem`);
       if (c > 0) {
         throw "OOOOPS!!!";
       }
-      console.log(`${c} == ${n}`);
+      log(`${c} == ${n}`);
       c += 1;
     }
   } catch (e) {
-    console.log("TRAPPED in test", e);
+    log("TRAPPED in test", e);
   }
+
+  expect(log.output).toEqual([
+    "begin",
+    "GOT 0 elem",
+    "0 == 0",
+    "middle",
+    "GOT 1 elem",
+    "finally",
+    "TRAPPED in test",
+    "OOOOPS!!!"
+  ]);
 });
 
-test("as", async () => {
-  async function* x(g: (_: number) => number = _ => _): AsyncGenerator<number> {
-    console.log("begin");
-    yield g(0);
-    console.log("end");
-    return "yes";
+test("generator with return statement has expected order of operations", async () => {
+  const log = logger();
+  async function* generator(): AsyncGenerator<number> {
+    log("generator enters");
+    yield 1;
+    log("generator returns");
+    return "result";
   }
 
-  const a = x();
+  log("before calling generator");
+  const a = generator();
+  log("before first call to next");
+  log(await a.next());
+  log("before second call to next");
+  log(await a.next());
+  log("after consuming generator");
 
-  console.log("next1");
-  console.log(await a.next());
-  console.log("next2");
-  console.log(await a.next());
-  console.log("donee");
+  expect(log.output).toEqual([
+    "before calling generator",
+    "before first call to next", // the async generator function does not start until the first call to next()
+    "generator enters",
+    { value: 1, done: false },
+    "before second call to next",
+    "generator returns",
+    { value: "result", done: true },
+    "after consuming generator",
+  ]);
+});
+
+test("generator consumed via for loop has return value ignored", async () => {
+  const log = logger();
+  async function* generator(): AsyncGenerator<number> {
+    log("generator enters");
+    yield 1;
+    log("generator returns");
+    return "result";
+  }
+
+  const b = generator();
+  for await (const x of b) {
+    log(x);
+  }
+  log("after for-await");
+  log(await b.next());
+
+  expect(log.output).toEqual([
+    "generator enters",
+    1,
+    "generator returns",
+    "after for-await",
+    { value: undefined, done: true }, // the return value is lost when iterator is consumed using "for [await] of"
+  ]);
+})
+
+test("some() consumes iterator", async () => {
+  // See https://github.com/tc39/proposal-iterator-helpers?tab=readme-ov-file#somefn
+});
+
+test("map() concurrent helpers - results are computed concurrently", async () => {
+  // See https://github.com/tc39/proposal-async-iterator-helpers?tab=readme-ov-file#concurrency
+});
+
+test("map() passing the protocol", async () => {
+  // See https://github.com/tc39/proposal-async-iterator-helpers/blob/main/DETAILS.md#passing-the-protocol
 });
