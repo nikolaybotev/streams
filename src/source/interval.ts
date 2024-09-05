e7b1720505fcc076e0700052b3a4702390dbd66b
Nikolay Botev <nikolaybotev.b@gmail.com>
2024-09-05 17:09:47 +0300

    Add Scheduler abstraction to interval source

--- src/source/interval.ts
+++ src/source/interval.ts
@@ -1,18 +1,33 @@
 import { makePipe } from "../util/pipe";
 
+export interface IntervalScheduler<T = unknown> {
+  schedule(interval: number, listener: () => unknown): T;
+  cancel(scheduled: T);
+}
+
+export const defaultScheduler = {
+  schedule(interval: number, listener: () => unknown) {
+    return setInterval(listener, interval);
+  },
+  cancel(scheduled) {
+    clearInterval(scheduled as number);
+  },
+} as IntervalScheduler;
+
 export function streamInterval(
   periodMillis: number,
+  scheduler: IntervalScheduler = defaultScheduler,
 ): AsyncIterableIterator<number> {
   const { next, ...pipe } = makePipe<number>();
 
   let n = 0;
-  const timer = setInterval(() => {
+  const timer = scheduler.schedule(periodMillis, () => {
     pipe.put(n++);
-  }, periodMillis);
+  });
 
   function close(): Promise<IteratorResult<number>> {
     pipe.close(true);
-    clearInterval(timer);
+    scheduler.cancel(timer);
     return Promise.resolve({ done: true, value: undefined });
   }
 
