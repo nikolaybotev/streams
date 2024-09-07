82ba9a2b661aa49a571a45c0e75a293f20bd021b
Nikolay Botev <nikolaybotev.b@gmail.com>
2024-09-07 12:50:27 +0300

    Replace readable-chunks with the new PassThrough stream implementation

--- src/sources/readable-chunks.ts
+++ src/sources/readable-chunks.ts
@@ -1,48 +1,23 @@
-import { Readable, Writable } from "node:stream";
-import { makeAsyncGeneratorPair } from "../util/async-iterator-pair";
-import { makeLazyAsyncGenerator } from "./async-generator";
+import { PassThrough, Readable } from "node:stream";
 
-export function readableChunks(
+export async function* readableChunks(
   readable: Readable,
   encoding?: BufferEncoding,
-): AsyncGenerator<Buffer> {
-  const [consumer, producer] = makeAsyncGeneratorPair<Buffer>(stop, stop);
-
-  const writable = new Writable({
-    defaultEncoding: encoding,
-    write(chunk: Buffer, _encoding, callback) {
-      const put = producer.next(chunk);
-
-      // Apply back-pressure by awaiting consumption of produced items...
-      put.then(() => callback());
-    },
-  });
-
-  const errorListener = (error) => {
-    producer.throw!(error);
-  };
-
-  const endListener = () => {
-    // Allow consumers to read any buffered data
-    producer.return!();
-  };
-
-  function start() {
-    // Use pipe instead of on("data") because unpipe() releases the stream,
-    // putting the stream in paused mode.
-    // See https://nodejs.org/api/stream.html#two-reading-modes
-    readable.pipe(writable);
-    readable.on("end", endListener);
-    readable.on("close", endListener);
-    readable.on("error", errorListener);
+): AsyncGenerator<Buffer, void, undefined> {
+  if (encoding !== undefined) {
+    readable.setEncoding(encoding);
   }
 
-  function stop() {
-    readable.unpipe(writable);
-    readable.off("end", endListener);
-    readable.off("close", endListener);
-    readable.off("error", errorListener);
-  }
+  const passThrough = new PassThrough();
 
-  return makeLazyAsyncGenerator(start, consumer);
+  // Pipe to a passthrough so as not to destroy the readable stream when the
+  // AsyncGenerator is interrupted before reaching the end of the stream.
+  readable.pipe(passThrough);
+  try {
+    yield* passThrough;
+  } finally {
+    // Detach from the readable stream - unpipe is not called automatically
+    // when the passThrough stream is destroyed.
+    readable.unpipe(passThrough);
+  }
 }
