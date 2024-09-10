e0f08c182c186743407a8ad37f802b1e20370827
Nikolay Botev <nikolaybotev.b@gmail.com>
2024-09-10 22:45:36 +0300

    Update to TypeScript 5.6.2

--- src/factories/async-iterator.ts
+++ src/factories/async-iterator.ts
@@ -1,19 +1,15 @@
 import { AsyncIteratorStream } from "../async-iterator-stream";
 
 declare global {
-  interface AsyncGenerator<T> {
-    stream(): AsyncIteratorStream<T>;
-  }
-
-  interface AsyncIterableIterator<T> {
+  interface AsyncIteratorObject<T> {
     stream(): AsyncIteratorStream<T>;
   }
 }
 
-const AsyncIteratorPrototype = Object.getPrototypeOf(
+const AsyncIteratorObjectPrototype = Object.getPrototypeOf(
   Object.getPrototypeOf(Object.getPrototypeOf((async function* () {})())),
 );
 
-AsyncIteratorPrototype.stream = function () {
+AsyncIteratorObjectPrototype.stream = function () {
   return AsyncIteratorStream.from(this);
 };
--- src/factories/iterator.ts
+++ src/factories/iterator.ts
@@ -2,25 +2,20 @@ import { AsyncIteratorStream } from "../async-iterator-stream";
 import { IteratorStream } from "../iterator-stream";
 
 declare global {
-  interface Generator<T> {
-    stream(): IteratorStream<T>;
-    streamAsync(): AsyncIteratorStream<T>;
-  }
-
-  interface IterableIterator<T> {
+  interface IteratorObject<T> {
     stream(): IteratorStream<T>;
     streamAsync(): AsyncIteratorStream<T>;
   }
 }
 
-const IterableIteratorPrototype = Object.getPrototypeOf(
+const IteratorObjectPrototype = Object.getPrototypeOf(
   Object.getPrototypeOf([].values()),
 );
 
-IterableIteratorPrototype.stream = function () {
+IteratorObjectPrototype.stream = function () {
   return IteratorStream.from(this);
 };
 
-IterableIteratorPrototype.streamAsync = function () {
+IteratorObjectPrototype.streamAsync = function () {
   return AsyncIteratorStream.from(this);
 };
